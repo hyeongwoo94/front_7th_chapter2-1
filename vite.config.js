e5acbb493e326d60a7eadb4482778510555f3bc3
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2025-11-09 17:33:57 +0900

    로컬/배포서버 두개 동시에 사용하기

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  // 배포할때는  이 주석을 풀어줘야 한다.
-  // base: "/front_7th_chapter2-1/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/front_7th_chapter2-1/",
   test: {
     globals: true,
     environment: "jsdom",
@@ -14,4 +13,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
