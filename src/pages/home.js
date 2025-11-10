0624990ab5c277ddad5155add98a22663645f7e5
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2025-11-10 21:51:40 +0900

    발제따라하기

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,11 @@
 import { Layout } from "../components/Layout.js";
 import { Search } from "../components/Search.js";
 import { ItemList } from "../components/ItemList.js";
-
-export const Home = () => {
+export const Home = ({ filters, pagination, products, loading }) => {
   return `
     ${Layout({
-      top: Search(),
-      main: ItemList(),
+      top: Search({ filters, pagination }),
+      main: ItemList({ products, loading }),
     })}
   `;
 };
